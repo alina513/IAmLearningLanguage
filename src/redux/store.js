53e9029fa63f352d25ce4bfcdccd59a925651cb7
alina513 <130853290+alina513@users.noreply.github.com>
2024-05-08 14:58:28 +0300

    CommentPersist

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,12 @@ import authReducer from './authSlice';
 import {
   persistStore,
   persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
+  // FLUSH,
+  // REHYDRATE,
+  // PAUSE,
+  // PERSIST,
+  // PURGE,
+  // REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 const authPersistConfig = {
